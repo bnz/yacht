6c584f87254c18869675f094903577d4d914bf44
bonez <cheekbonez@gmail.com>
2020-03-18 22:18:15 +0200

    useDispatch to useDispatchedCallback

--- src/components/Drawer/LangChangeButtons/LangChangeButtons.tsx
+++ src/components/Drawer/LangChangeButtons/LangChangeButtons.tsx
@@ -3,17 +3,17 @@ import { DrawerListDivider } from '../DrawerListDivider'
 import { DrawerListItem } from '../DrawerListItem'
 import { ButtonGroup } from '../DiceSizeChanger/ButtonGroup'
 import Button from '@material-ui/core/Button'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { makeLanguageSelector } from '../../../redux/selectors/makeLanguageSelector'
 import { i18n } from '../../../helpers/i18n/i18n'
 import { changeLanguageThunk } from '../../../redux/actions/changeLanguageThunk'
+import { useDispatchedCallback } from '../../../helpers/useDispatchedCallback'
 
 const languageSelector = makeLanguageSelector()
 
 export const LangChangeButtons: FC = () => {
   const language = useSelector(languageSelector)
-  const dispatch = useDispatch()
-  const onClick = () => dispatch(changeLanguageThunk())
+  const onClick = useDispatchedCallback(changeLanguageThunk())
 
   return (
     <>
