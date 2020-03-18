40daea4701c0ee71dbcbbaf7ae9a425bb850dbac
bonez <cheekbonez@gmail.com>
2020-03-18 22:13:02 +0200

    visibilityHOC_2 - refactored (typed) visibilityHOC

--- src/helpers/visibilityHOC.tsx
+++ src/helpers/visibilityHOC.tsx
@@ -1,9 +1,12 @@
-import React, { FC } from 'react'
+import React, { ComponentType, FC } from 'react'
 
 export interface VisibilityHOCProps {
   hidden: boolean
 }
 
+/**
+ * @deprecated
+ */
 export const visibilityHOC = (Component: any /*<--TODO*/): FC<VisibilityHOCProps> => ({ hidden, ...restProps }) => {
 
   if (hidden) {
@@ -12,5 +15,22 @@ export const visibilityHOC = (Component: any /*<--TODO*/): FC<VisibilityHOCProps
 
   return (
     <Component {...restProps} />
+  )
+}
+
+
+export const visibilityHOC_2 = <T extends {}>(
+  WrappedComponent: ComponentType<T>,
+): FC<T & VisibilityHOCProps> => ({
+  hidden,
+  ...restProps
+}) => {
+
+  if (hidden) {
+    return null
+  }
+
+  return (
+    <WrappedComponent {...restProps as T} />
   )
 }
