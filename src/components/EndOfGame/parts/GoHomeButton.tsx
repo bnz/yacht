db7e77f7aec2cfab1998e0a0fed7118c015a7d75
bonez <cheekbonez@gmail.com>
2020-03-19 10:41:45 +0200

    GoHomeButton - useDispatchedCallback

--- src/components/EndOfGame/parts/GoHomeButton.tsx
+++ src/components/EndOfGame/parts/GoHomeButton.tsx
@@ -1,14 +1,12 @@
 import React, { FC } from 'react'
 import Button from '@material-ui/core/Button'
 import { i18n } from '../../../helpers/i18n/i18n'
-import { useDispatch } from 'react-redux'
-import { ThunkAction2 } from '../../../helpers/types'
 import { restartGameThunk } from '../../../redux/actions/restartGameThunk'
 import HomeIcon from '@material-ui/icons/Home'
+import { useDispatchedCallback } from '../../../helpers/useDispatchedCallback'
 
 export const GoHomeButton: FC = () => {
-  const dispatch = useDispatch<ThunkAction2>()
-  const onClick = () => dispatch(restartGameThunk())
+  const onClick = useDispatchedCallback(restartGameThunk())
 
   return (
     <Button
